ce743c66a71c7e1d3256de57b193872bd7c6dcb7
Ilinca Obadescu <Ilinca.obadescu@gmail.com>
2024-11-13 21:50:38 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,55 +1,15 @@
-// vite.config.js
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   base: '/11/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@data': path.resolve(__dirname, './src/data'),
-    }
-  },
-  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg'],
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    cssCodeSplit: true,
-    sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    },
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          utils: ['@headlessui/react', 'lucide-react']
-        },
-        assetFileNames: (assetInfo) => {
-          const extType = assetInfo.name.split('.').at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            return `assets/images/[name][extname]`;
-          }
-          if (/css/i.test(extType)) {
-            return 'assets/css/[name][extname]';
-          }
-          return `assets/[name][extname]`;
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js'
-      },
-      input: {
-        main: path.resolve(__dirname, 'index.html')
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
       }
     }
   }
